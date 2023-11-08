9f48edac117767920ff668a1cbddae61b5ca9710
park0jae <okvv26@naver.com>
2023-11-08 16:02:19 +0900

    yml refactor

--- src/main/java/com/zerozae/exhibition/domain/file/service/FileService.java
+++ src/main/java/com/zerozae/exhibition/domain/file/service/FileService.java
@@ -1,42 +1,38 @@
 package com.zerozae.exhibition.domain.file.service;
 
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.zerozae.exhibition.domain.file.exception.FileUploadFailureException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.annotation.PostConstruct;
-import java.io.File;
 import java.io.IOException;
 
-@Service
+@Component
+@RequiredArgsConstructor
 public class FileService {
 
-    @Value("${upload.image.location}")
-    private String location; // 파일을 업로드할 위치 주입
+    @Value("${cloud.aws.s3.bucket}")
+    private String BUCKET;
+    private final AmazonS3Client amazonS3Client;
 
-    @PostConstruct
-    void postConstruct() { // 파일 업로드 될 디렉토리가 없을경우 생성
-        File dir = new File(location);
-        if (!dir.exists()) {
-            dir.mkdir();
-        }
-    }
-
-    public String getFullPath(String fileName) {
-        return location + fileName;
-    }
-
-    public void upload(MultipartFile file, String fileName) {
+    public void upload(MultipartFile file, String filename) {
         try {
-            file.transferTo(new File(location + fileName));
-        } catch (IOException e) {
+            ObjectMetadata metadata= new ObjectMetadata();
+            metadata.setContentType(file.getContentType());
+            metadata.setContentLength(file.getSize());
+            amazonS3Client.putObject(BUCKET, filename, file.getInputStream(), metadata);
+        } catch(IOException e) {
             throw new FileUploadFailureException();
         }
     }
 
-    public void delete(String filename) {
-        new File(location + filename).delete();
+    public void delete(String fileName){
+        DeleteObjectRequest request = new DeleteObjectRequest(BUCKET , fileName);
+        amazonS3Client.deleteObject(request);
     }
 }
--- src/main/java/com/zerozae/exhibition/global/config/cors/CorsConfig.java
+++ src/main/java/com/zerozae/exhibition/global/config/cors/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.zerozae.exhibition.global.config;
+package com.zerozae.exhibition.global.config.cors;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
